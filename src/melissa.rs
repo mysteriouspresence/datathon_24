ba1f4b63d0abaaedd3056fc33c84d0931aa63ec9
chelsea <chelseawen03@gmail.com>
2024-04-13 11:46:27 -0700

    fixed api key thing

--- src/melissa.rs
+++ src/melissa.rs
@@ -3,11 +3,12 @@
 use dotenv::dotenv;
 use reqwest;
 use std::error::Error;
-static TARGET: &str = "https://businesscoder.melissadata.net/WEB/BusinessCoder/doBusinessCoderUS?idJ3xzwBYgVfflfnpUlIwQVo**&city=Irvine&state=CA";
+static TARGET: &str = "https://businesscoder.melissadata.net/WEB/BusinessCoder/doBusinessCoderUS?id=";
 pub async fn params() -> Vec<String> {
     dotenv().ok();
-    //let api_key = std::env::var("MELISSA_API_KEY").expect("MELISSA_API_KEY must be set");
-    let body = reqwest::get(TARGET).await.unwrap().text().await.unwrap();
+    let api_key = std::env::var("MELISSA_API_KEY").expect("MELISSA_API_KEY must be set");
+    let link = format!("{}{}", TARGET, api_key);
+    let body = reqwest::get(link).await.unwrap().text().await;
     println!("Response: {:?}", body);
-    return vec!["ur".to_string(), "mom".to_string()];
+    return vec![api_key];
 }
