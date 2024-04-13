ab7ee5ce6f5dd0971d7c09cee7e0388bd3205ab4
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-04-13 10:49:02 -0700

    boiler plate code setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod melissa;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/melissa.rs
@@ -0,0 +1,7 @@
+use reqwest;
+use std::error::Error;
+use dotenv::dotenv;
+static TARGET: &str = "MelissaAPILINK";
+async fn params() -> Vec<String> {
+
+}
