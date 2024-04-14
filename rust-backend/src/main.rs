a239f097e67d3e49bccedee3fdc381ea76c86c50
chelsea <chelseawen03@gmail.com>
2024-04-13 21:06:03 -0700

    saving level hierarchy

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -1,9 +1,8 @@
 use calamine::*;
-use std::{collections::HashMap, ops::Deref};
+use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 struct Node {
-    level: u8,
     part: String,
     variant: String,
     is_error: bool,
@@ -17,14 +16,14 @@ impl Node {
 }
 
 struct Assembly {
-    parts: HashMap<String, Node>,
+    parts: HashMap<String, (u8, Node)>,
 }
 impl Assembly {
     fn set_to_affected(&mut self, target: String) {
         for (_, n) in self.parts.iter_mut() {
             //println!("part{} and target {}", n.part, target);
-            if n.part == target {
-                n.set_to_affected();
+            if n.1.part == target {
+                n.1.set_to_affected();
             }
         }
     }
@@ -34,7 +33,7 @@ fn main() -> Result<(), Error> {
     let mut workbook: Xlsx<_> = open_workbook(PATH)?;
     let range = workbook.worksheet_range("raw")?;
 
-    let results: HashMap<String, Node> = RangeDeserializerBuilder::new()
+    let results: HashMap<String, (u8,Node)> = RangeDeserializerBuilder::new()
         .from_range(&range)?
         .map(|row| {
             let (level, path, _, _, part, _, _, _, _, _, variant, is_error): (
@@ -53,14 +52,14 @@ fn main() -> Result<(), Error> {
             ) = row.unwrap_or_default();
             (
                 part.clone(),
+                (level,
                 Node {
-                    level,
                     part,
                     variant,
                     is_error,
                     is_affected: false,
                     path,
-                },
+                })
             )
         })
         .collect();
@@ -69,8 +68,8 @@ fn main() -> Result<(), Error> {
     let mut results = Assembly { parts: results };
     let mut count = 0;
     for node in node_parser {
-        let path_nodes: Vec<String> = node.1.path.split("-").map(|x| x.to_string()).collect();
-        if node.1.is_error {
+        let path_nodes: Vec<String> = node.1.1.path.split("-").map(|x| x.to_string()).collect();
+        if node.1.1.is_error {
             for id in path_nodes.clone() {
                 results.set_to_affected(id);
             }
@@ -78,6 +77,6 @@ fn main() -> Result<(), Error> {
         count = count + 1;
     }
     println!("{:?}", results.parts);
-    results.parts.iter().
+    //results.parts.iter().
     Ok(())
 }
