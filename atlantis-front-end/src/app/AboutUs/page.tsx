746bbec13984fe1c3deec4259a18bdc2494ba9a0
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-04-14 10:08:10 -0700

    Remove unused imports in AboutUs page

--- atlantis-front-end/src/app/AboutUs/page.tsx
+++ atlantis-front-end/src/app/AboutUs/page.tsx
@@ -9,16 +9,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+
 import { HeroParallax } from "./Ourwork";
 const products = [
   {
